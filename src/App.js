9715540400530600509e3dcccb4dd19fd22dad72
Joary13 <jolea13008@gmail.com>
2023-12-28 11:48:33 +0100

    npm server package + data fetching

--- src/App.js
+++ src/App.js
@@ -1,6 +1,41 @@
+import React, { useEffect, useReducer } from 'react';
+import ReactDOM from 'react-dom/client';
+
 import Header from './Header.js';
 import Main from './Main.js';
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, question: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    default:
+      throw new Error('Action unknown');
+  }
+}
+
+const initialState = {
+  question: [],
+  // "loading","error","ready","active","finished"
+  status: 'loading',
+};
 function App() {
+  const [state, dispatch] = useReducer(reducer, { initialState });
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const res = await fetch('http://localhost:8000/questions');
+        const data = await res.json();
+        dispatch({ type: 'dataReceived', payload: data });
+      } catch (err) {
+        dispatch({ type: 'dataFailed' });
+        console.error('Error');
+      }
+    }
+    fetchData();
+  }, []);
   return (
     <div className='app'>
       <Header />
--- src/Main.js
+++ src/Main.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 function Main({ children }) {
   return <main className='main'>{children}</main>;
 }
